b727a82624f9172d8ca7c027af002d9b89d9911b
antoine moynault <antmo654@gmail.com>
2021-12-18 18:35:25 +0100

    restart 20-(0..9) rb

--- adv20.rb
+++ adv20.rb
@@ -2,15 +2,9 @@
 
 # ##############################################################################
 
-def debug(*args)
-  puts "debug: " + args.map(&:to_s).join(" ")
-  args.length == 1 ? args.first : args
-end
-
-# ##############################################################################
-
 # load "adv20.rb" ; d20011()
 
+# ##############################################################################
 
 # ###########################################################################
 #
@@ -18,28 +12,15 @@ end
 #
 # ###########################################################################
 
-def input2009()
-  IO.readlines('inputs/2009-0.in')
-  .map(&:to_i)
-end
-
 # 69316178
 def d20091()
-  input, prmbl = input2009, 25
-  prmbl
-    .upto(input.length)
-    .detect { |i|
-    break input[i] if (
-        input.slice(i - prmbl, prmbl)
-          .combination(2)
-          .detect{|a, b| a + b == input[i]}.nil?)}
+  numbs, prmbl =
+    input(2009).split("\n").map(&:to_i), 25
+
+  numbs.each_cons(prmbl + 1) { |*lst, nb|
+    lst.combination(2).detect { |a, b| a + b == nb } || (break nb) }
 end
 
-def d20092()
-  input = input2009
-  target = 127
-
-end
 
 # ###########################################################################
 #
@@ -47,20 +28,12 @@ end
 #
 # ###########################################################################
 
-def input2008()
-  IO.readlines('inputs/2008.in', chomp: true)
-  .map(&:split)
-  .map.with_index{|(opc, arg), i| [i, [opc, arg.to_i]]}
-  .to_h
-end
-
-def loop2008(input)
+def loop2008(progr)
   pc, acc, seen = 0, 0, []
-  while true do
-    break if seen.member?(pc)
+  loop do
+    break if seen.member?(pc) or not progr.member?(pc)
     seen.push(pc)
-    break if not input.member?(pc)
-    opc, arg = input[pc]
+    opc, arg = progr[pc]
     if opc == "nop" then
       pc += 1
     elsif opc == "acc" then
@@ -68,30 +41,36 @@ def loop2008(input)
       pc += 1
     elsif opc == "jmp" then
       pc += arg
-    else fail
-    end
+    else fail end
   end
   return acc, pc
 end
 
 # 2080
 def d20081()
-  loop2008(input2008).first
+  progr =
+    input(2008).split("\n").map(&:split)
+      .map.with_index { |(opc, arg), i| [ i, [opc, arg.to_i] ] }
+      .to_h
+
+  loop2008(progr).first
 end
 
 # 2477
 def d20082()
-  input = input2008()
-  input
-    .find{|i, (opc, arg)|
+  progr =
+    input(2008).split("\n").map(&:split)
+      .map.with_index { |(opc, arg), i| [ i, [opc, arg.to_i] ] }
+      .to_h
+
+  progr.find { |i, (opc, arg)|
     if (opc == "nop")
-      acc, pc = loop2008(input.merge({i => ["jmp", arg]}))
-      break acc if not input.member?(pc)
+      acc, pc = loop2008(progr.merge({ i => ["jmp", arg] }))
+      break acc if not progr.member?(pc)
     elsif (opc == "jmp")
-      acc, pc = loop2008(input.merge({i => ["nop", arg]}))
-      break acc if not input.member?(pc)
-    else nil end
-  }
+      acc, pc = loop2008(progr.merge({ i => ["nop", arg] }))
+      break acc if not progr.member?(pc)
+    else nil end }
 end
 
 
@@ -111,31 +90,36 @@ end
 # faded blue bags contain no other bags.
 # dotted black bags contain no other bags.
 
-def input2007()
-  IO.readlines('inputs/2007.in', chomp: true)
-    .map{ |l|
-      src = l.scan(/^\w+ \w+/)
-      dst = l.scan(/(\d+) (\w+ \w+)/).map{|n, b| [n.to_i, b]}
-      [src.first, dst] }
-    .to_h
-end
-
 # 229
 def d20071()
-  def isvalid?(input, color)
-    (color == "shiny gold") or (input[color]&.find{|_, c| isvalid?(input, c)})
+  rules = input(2007).split("\n")
+    .map { |l|
+      src = l.scan(/^\w+ \w+/).first
+      dst = l.scan(/(\d+) (\w+ \w+)/).map { |n, b| [n.to_i, b] }
+      [src, dst] }.to_h
+
+  def isvalid?(rules, color)
+    (color == "shiny gold") or rules[color].find { |_, c| isvalid?(rules, c) }
   end
-  input2007
-    .filter{|c, _| isvalid?(input2007, c) }
+
+  rules
+    .filter { |c, _| isvalid?(rules, c) }
     .length - 1
 end
 
 # 6683
 def d20072()
-  def nbbags(input, color)
-    input[color].map{|n, c| (n * (1 + nbbags(input, c)))}.sum
+  rules = input(2007).split("\n")
+    .map { |l|
+      src = l.scan(/^\w+ \w+/).first
+      dst = l.scan(/(\d+) (\w+ \w+)/).map { |n, b| [n.to_i, b] }
+      [src, dst] }.to_h
+
+  def nbbags(rules, color)
+    rules[color].map { |n, c| n * (1 + nbbags(rules, c)) }.sum
   end
-  nbbags(input2007, "shiny gold")
+
+  nbbags(rules, "shiny gold")
 end
 
 
@@ -145,23 +129,18 @@ end
 #
 # ###########################################################################
 
-def input2006()
-  IO.read('inputs/2006.in')
-  .split("\n\n")
-end
-
 # 6947
 def d20061()
-  input2006()
-    .map{|l| l.split.join.chars.uniq.length }
+  input(2006).split("\n\n")
+    .map { |l| l.split.join.chars.uniq.length }
     .sum
 end
 
 # 3398
 def d20062()
-  input2006()
-    .map{|l| l.scan(/\w+/).map{|s| s.chars}}
-    .map{|l| l.reduce(:intersection).length }
+  input(2006).split("\n\n")
+    .map { |l| l.scan(/\w+/).map(&:chars) }
+    .map { |l| l.reduce(:intersection).length }
     .sum
 end
 
@@ -171,10 +150,6 @@ end
 # 2020 DAY 05
 #
 # ###########################################################################
-
-def input2005()
-  IO.read('inputs/2005.in').lines(chomp: true)
-end
 
 def decode2005(s)
   (r1, r2), (s1, s2) = [0, 127], [0, 7]
@@ -188,28 +163,21 @@ def decode2005(s)
     end
   end
   fail unless (r1 == r2) and (s1 == s2)
-  return r1, s1
+  return r1 * 8 + s1
 end
-
-def seatid((row, seat)) row * 8 + seat end
 
 # 947
 def d20051()
-  input2005()
-    .map{|s| seatid(decode2005(s)) }
+  input(2005).split("\n").map { |s| decode2005(s) }
     .max
 end
 
 # 636
 def d20052()
-  boarded = input2005.map{|s| seatid(decode2005(s)) }
+  boarded =
+    input(2005).split("\n").map { |s| decode2005(s) }
   ((boarded.min...boarded.max).to_a - boarded).first
 end
-
-# (1..126).to_a.product((1..7).to_a)
-#   .map{|r, s| seatid([r, s]) }
-#   .find{|sid| (not boarded.include?(sid) and boarded.include?(sid - 1) and
-#                boarded.include?(sid + 1)) }
 
 
 # ###########################################################################
@@ -217,12 +185,6 @@ end
 # 2020 DAY 04
 #
 # ###########################################################################
-
-def input2004()
-  IO.read('inputs/2004.in')
-  .split("\n\n")
-  .map{|l| l.scan(/\S+/).map{|i| i.split(":")}.to_h }
-end
 
 # byr (Birth Year)
 # iyr (Issue Year)
@@ -235,10 +197,14 @@ end
 
 # 196
 def d20041()
-  required = [
-    "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" ]
-  input2004
-    .filter{|h| required.difference(h.keys).empty? }
+  passes =
+    input(2004).split("\n\n")
+      .map { |l| l.scan(/\S+/).map { |i| i.split(":") }.to_h }
+  required =
+    [ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" ]
+
+  passes
+    .filter { |h| required.difference(h.keys).empty? }
     .length
 end
 
@@ -255,8 +221,11 @@ end
 
 # 114
 def d20042()
-  required = [
-    "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" ]
+  passes =
+    input(2004).split("\n\n")
+      .map { |l| l.scan(/\S+/).map { |i| i.split(":") }.to_h }
+  required =
+    [ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" ]
 
   def isvalid(k, v)
     case k
@@ -281,9 +250,9 @@ def d20042()
     end
   end
 
-  input2004
-    .filter{|h| required.difference(h.keys).empty? }
-    .filter{|h| h.all?{|k, v| isvalid(k, v)} }
+  passes
+    .filter { |h| required.difference(h.keys).empty? }
+    .filter { |h| h.all? { |k, v| isvalid(k, v) } }
     .length
 end
 
@@ -294,26 +263,28 @@ end
 #
 # ###########################################################################
 
-def input2003()
-  IO.readlines('inputs/2003.in', chomp: true)
-end
-
 # 254
 def d20031()
-  input = input2003()
-  xmax, ymax = input.first.length, input.length
-  slope_x, slope_y = 3, 1
-  x, y, trees = 0, 0, 0
-  while y < ymax do
-    trees += 1 if (input[y][x % xmax] == '#')
-    x, y = x + slope_x, y + slope_y
+  input =
+    input(2003).split("\n")
+  xmax, ymax =
+    input.first.length, input.length
+
+  traverse = lambda do |slope_x, slope_y|
+    x, y, trees = 0, 0, 0
+    while y < ymax do
+      trees += 1 if (input[y][x % xmax] == '#')
+      x, y = x + slope_x, y + slope_y
+    end
+    trees
   end
-  trees
+
+  traverse.call(3, 1)
 end
 
 # 1666768320
 def d20032()
-  input = input2003()
+  input = input(2003).split("\n")
   xmax, ymax = input.first.length, input.length
 
   traverse = lambda do |slope_x, slope_y|
@@ -326,7 +297,7 @@ def d20032()
   end
 
   [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-    .map{|slope| traverse.call(*slope) }
+    .map { |slope| traverse.call(*slope) }
     .reduce(:*)
 end
 
@@ -337,24 +308,21 @@ end
 #
 # ###########################################################################
 
-def input2002()
-  IO.readlines('inputs/2002.in').map{|l| l.scan(/\w+/)}
-end
-
 # 546
 def d20021()
-  input2002
-    .filter{|lo, hi, c, s| (
-              s.count(c).between?(lo.to_i, hi.to_i) )}
+  input(2002)
+    .split("\n").map { |l| l.scan(/\w+/) }
+    .filter { |lo, hi, c, s|
+      (s.count(c).between?(lo.to_i, hi.to_i) ) }
     .length
-  # (lo.to_i .. hi.to_i).include?(s.count(c))
 end
 
 # 275
 def d20022()
-  input2002
-    .filter{|lo, hi, c, s| (
-              (s[lo.to_i - 1] == c) ^ (s[hi.to_i - 1] == c)) }
+  input(2002)
+    .split("\n").map { |l| l.scan(/\w+/) }
+    .filter { |lo, hi, c, s|
+      (s[lo.to_i - 1] == c) ^ (s[hi.to_i - 1] == c) }
     .length
 end
 
@@ -365,25 +333,51 @@ end
 #
 # ###########################################################################
 
-def input2001()
-  IO.read('inputs/2001.in').split.map(&:to_i)
-end
-
 # 605364
 def d20011()
-  input2001
-  .combination(2)
-  .find{|comb| comb.sum == 2020}
-  .reduce(:*)
+  input(2001).split.map(&:to_i)
+    .combination(2)
+    .find { |comb| comb.sum == 2020 }
+    .reduce(:*)
 end
 
 # 128397680
 def d20012()
-  input2001
+  input(2001).split.map(&:to_i)
     .combination(3)
-    .find{|comb| comb.sum == 2020}
+    .find { |comb| comb.sum == 2020 }
     .reduce(:*)
 end
 
 
 # ##############################################################################
+
+def input(id)
+  IO.read("#{File.dirname(__FILE__)}/inputs/#{id}.in")
+end
+
+def debug(*args)
+  puts "debug: " + args.map(&:to_s).join(" ")
+  args.length == 1 ? args.first : args
+end
+
+
+# ##############################################################################
+
+fail if RUBY_VERSION.to_f < 2.7
+
+if $PROGRAM_NAME == __FILE__
+
+  proc, *args = ARGV
+
+  proc ||= private_methods.filter {_1.to_s.match?(/d\d{5}/) }.sort.last
+
+  debug("call #{proc} ( #{args} )")
+
+  fstr = Time.now
+  p self.send(proc.to_sym, *args)
+  fend = Time.now
+
+  debug("took #{fend - fstr}s")
+
+end
