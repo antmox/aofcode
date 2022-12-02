4929b2a9d4b7c9412c56b977b54d63efafca74c1
Antoine Moynault <antoine.moynault@linaro.org>
2022-12-02 09:27:40 +0100

    22-02

--- adv22.rb
+++ adv22.rb
@@ -5,7 +5,76 @@
 
 # ##############################################################################
 #
-# 2021 DAY 1
+# 2022 DAY 2
+#
+# ##############################################################################
+
+# rock      0 defeats 2   scissors
+# paper     1 defeats 0   rock
+# scissors  2 defeats 1   paper
+
+# shape   : 1 for Rock, 2 for Paper, and 3 for Scissors
+# outcome : 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+# 10718
+def d22021()
+  input(2202)
+    .split("\n").map(&:split)
+    .map { | p1, p2 |
+      case [p1, p2]
+      when ["A", "X"] then 1 + 3 # rock vs rock
+      when ["A", "Y"] then 2 + 6 # rock vs papr
+      when ["A", "Z"] then 3 + 0 # rock vs scis
+      when ["B", "X"] then 1 + 0 # papr vs rock
+      when ["B", "Y"] then 2 + 3 # papr vs papr
+      when ["B", "Z"] then 3 + 6 # papr vs scis
+      when ["C", "X"] then 1 + 6 # scis vs rock
+      when ["C", "Y"] then 2 + 0 # scis vs papr
+      when ["C", "Z"] then 3 + 3 # scis vs scis
+      end
+  }.sum
+end
+
+# X means you need to lose
+# Y means you need to end the round in a draw
+# Z means you need to win
+
+# 14652
+def d22022()
+  input(2202)
+    .split("\n").map(&:split)
+    .map { | p1, p2 |
+      case [p1, p2]
+      when ["A", "X"] then 3 + 0 # lose vs rock -> scis
+      when ["B", "X"] then 1 + 0 # lose vs papr -> rock
+      when ["C", "X"] then 2 + 0 # lose vs scis -> papr
+      when ["A", "Y"] then 1 + 3 # draw vs rock -> rock
+      when ["B", "Y"] then 2 + 3 # draw vs papr -> papr
+      when ["C", "Y"] then 3 + 3 # draw vs scis -> scis
+      when ["A", "Z"] then 2 + 6 # win  vs rock -> papr
+      when ["B", "Z"] then 3 + 6 # win  vs papr -> scis
+      when ["C", "Z"] then 1 + 6 # win  vs scis -> rock
+      end
+  }.sum
+end
+
+# # P1
+# i1 = ["A", "B", "C"].index(p1)
+# i2 = ["X", "Y", "Z"].index(p2)
+# shape_pts = (i2 + 1)
+# outcm_pts = (i2 == ((i1 + 1) % 3)) ? 6 : (i2 == i1) ? 3 : 0
+
+# # P2
+# i1 = ["A", "B", "C"].index(p1)
+# i2 = (p2 == "X") ? ((i1 + 2) % 3) :
+#      (p2 == "Z") ? ((i1 + 1) % 3) : i1
+# shape_pts = (i2 + 1)
+# outcm_pts = (i2 == ((i1 + 1) % 3)) ? 6 : (i2 == i1) ? 3 : 0
+
+
+# ##############################################################################
+#
+# 2022 DAY 1
 #
 # ##############################################################################
 
